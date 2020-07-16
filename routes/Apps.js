9cb56ca55a2b9f690b61c9fb4a051c628c9e3d00
Catherine Balogun <catherinebalogun@Catherines-MBP-32.lan>
2020-07-16 15:16:36 -0400

    Reviewed chnages made

--- routes/Apps.js
+++ routes/Apps.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/apps', controllers.getApps)
 router.get('/apps/:id', controllers.getApp)
 router.post('/apps', controllers.createApp)
+router.put('/apps/:id', controllers.updateApp)
+router.delete('/apps/:id', controllers.deleteApp)
 
 module.exports = router
