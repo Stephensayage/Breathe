dd58d81e0753ede37448ea662f9c33afada9b3b3
Stephen Sayage <stephensayage@Stephens-MBP.home>
2020-07-16 12:45:03 -0400

    schemas for users and apps created

--- models/users.js
+++ models/users.js
@@ -1,1 +1,15 @@
-// other stuff
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    imgUrl: { type: String, required: true },
+    location: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('users', User)
