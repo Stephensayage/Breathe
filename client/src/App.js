36e69860421c07bfa47f9868b37725aa61de8f0d
Kaisha Jones <KaishaJones@Samys-MBP.home>
2020-07-20 11:45:27 -0400

    update to import

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import UserCreate from "./screens/UserCreate/UserCreate"
 import UserDisplay from "./screens/UserDisplay/UserDisplay"
-import { Route , Switch} from "react-router-dom"
+import { Route , Switch, Router} from "react-router-dom"
 import Confidentiality from "./screens/Confidentiality/Confidentiality"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Confidentiality from "../src/screens/Confidentiality/Confidentiality"
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/usercreate" component={UserCreate} />
-        <Route path="/userdisplay" component={UserDisplay} />
+        <Route exact path="/userdisplay" component={UserDisplay} />
+      
       </Switch> 
     </div>
   );
