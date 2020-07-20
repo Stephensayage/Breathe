93a3a3579db44a85f1009cb8f5b97f14e60bd2cc
Stephen Sayage <stephensayage@Stephens-MBP.home>
2020-07-20 14:28:38 -0400

    styling updated on the sign in and create acc page. Homepage updated. CreateAcc component created.

---
+++ client/src/components/CreateAcc/CreateAcc.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Link, Route, Redirect, withRouter, useParams } from 'react-router-dom';
+import { createUser } from '../../services/users'
+import './CreateAcc.css'
+
+
+function CreateAcc() {
+
+  let params = useParams()
+
+  let [userName, setUserName] = useState('');
+  let [userEmail, setUserEmail] = useState('');
+  let [userPassword, setUserPassword] = useState('')
+
+  let [created, changeCreated] = useState('no')
+  let [user, changeUser] = useState('')
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const useInfo = {
+      'name': userName,
+      'email': userEmail,
+      'password': userPassword,
+    }
+    const createdUser = await createUser(useInfo)
+    const userId = createdUser._id
+    changeUser(userId)
+  }
+
+  if (created === 'yes') {
+    return <Redirect to={`/userdisplay/${user}`} />
+  }
+
+  return (
+    <>
+      <form className="form-ctn" onSubmit={handleSubmit} >
+        <div className="inner-ctn card border-0 col-12 col-lg-6 login-card mt-2 hv-center">
+          <h4 className="create-title">Create Account</h4>
+          Name <input className="form-input" type="text" value={userName} name='name' onChange={(e) => setUserName(e.target.value)} />
+        Email <input className="form-input" type="text" value={userEmail} name='email' onChange={(e) => setUserEmail(e.target.value)} />
+        Password <input className="form-input" type="text" value={userPassword} name='password' onChange={(e) => setUserPassword(e.target.value)} />
+          <input className="submit" type="submit" value="Sign Up" />
+          <button className="to-profile" onClick={() => changeCreated('yes')}>Take me to my profile</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default CreateAcc
--- client/src/screens/UserCreate/UserCreate.jsx
+++ client/src/screens/UserCreate/UserCreate.jsx
@@ -1,57 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Signin from "../../components/shared/SignIn/Signin"
-import { Link, Route, Redirect, withRouter, useParams } from 'react-router-dom';
-import { createUser } from '../../services/users'
-import { getUser } from '../../services/users'
+import CreateAcc from "../../components/CreateAcc/CreateAcc"
+import { withRouter } from 'react-router-dom';
 import Layout from "../../components/shared/Layout/Layout"
 import './UserCreate.css'
 
 
 function UserCreate() {
 
-  let params = useParams()
-
-  let [userName, setUserName] = useState('');
-  let [userEmail, setUserEmail] = useState('');
-  let [userPassword, setUserPassword] = useState('')
-
-  let [created, changeCreated] = useState('no')
-  let [user, changeUser] = useState('')
-
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const useInfo = {
-      'name': userName,
-      'email': userEmail,
-      'password': userPassword,
-    }
-    const createdUser = await createUser(useInfo)
-    const userId = createdUser._id
-    changeUser(userId)
-  }
-
-  if (created === 'yes') {
-    return <Redirect to={`/userdisplay/${user}`} />
-  }
 
   return (
     <>
       <Layout>
         <img className="nav-img" src="https://i.imgur.com/NbDJy5H.jpg" />
         <div className="outer-ctn">
-          <div className="create-acc-ctn">
-            <form className="form-ctn" onSubmit={handleSubmit} >
-              <div >
-                Name <input type="text" value={userName} name='name' onChange={(e) => setUserName(e.target.value)} />
-        Email <input type="text" value={userEmail} name='email' onChange={(e) => setUserEmail(e.target.value)} />
-        Password <input type="text" value={userPassword} name='password' onChange={(e) => setUserPassword(e.target.value)} />
-                <input type="submit" value="Submit" />
-              </div>
-            </form>
-            <button onClick={() => changeCreated('yes')}>Take me to my profile and matches</button>
-          </div>
+          <CreateAcc />
           <Signin />
         </div>
       </Layout>
@@ -61,4 +24,3 @@ function UserCreate() {
 }
 
 export default withRouter(UserCreate)
-
