3d3ed582a57e4c4de88f2d2f2bd50cc07c85a38f
Kaisha Jones <KaishaJones@Samys-MBP.home>
2020-07-21 01:55:20 -0400

    syntax

---
+++ client/src/screens/Confidentiality/confidentiality.js
@@ -0,0 +1,25 @@
+import React from "react"
+import App from "./App"
+
+
+function Confidentiality() {
+  return (){
+
+    <div className="conatainer">
+      {/*<Layout />*/}
+      <header className="box box-1">Header</header>
+      <aside className="box box-2">LeftSide</aside>
+      <main className="box box-3">Main Content</main>
+      <aside className="box box-4">RightSide</aside>
+      <footer className="box box-5">Footer
+       <div className="box box-6">icon-1</div>
+        <div className="box box-7">icon-2</div>
+        <div className="box box-8">icon-3</div>
+        <div className="boc box-9">icon-4</div>
+      </footer>
+    </div>
+  }
+
+}
+
+export default Confidentiality
